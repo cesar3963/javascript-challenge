5f4991cd990525e3f900ca4e9f6e7023812e6a03
cesar3963 <67514872+cesar3963@users.noreply.github.com>
2020-11-29 02:22:19 -0500

    finished

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -3,17 +3,6 @@ var tableData = data;
 
 var tbody = d3.select("tbody");
 
-// tableData.forEach(function(UfoData) {
-//     var row = tbody.append("tr");
-
-//     Object.entries(UfoData).forEach(function([key,value]){
-//         var cell = row.append("td");
-//         cell.text(value);
-
-        
-//     });
-// });
-
 var button = d3.select("#filter-btn");
 
 button.on("click", function(){
@@ -21,14 +10,18 @@ button.on("click", function(){
 
     d3.event.preventDefault();
 
-    var inputElement = d3.select("#datetime");
+    var inputElement = d3.select("#entry");
     
     var inputvalue = inputElement.property("value");
 
     console.log(inputvalue);
   
 
-    var filteredData = tableData.filter(ufo => ufo.datetime === inputvalue);
+    var filteredData = tableData.filter(ufo => ufo.datetime === inputvalue||
+                                                ufo.city === inputvalue||
+                                                ufo.state === inputvalue||
+                                                ufo.country === inputvalue||
+                                                ufo.shape === inputvalue);
 
     console.log(filteredData); 
 
